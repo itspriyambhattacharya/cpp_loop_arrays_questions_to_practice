f74fbb3396d15ae3c1136661367530010c95808f
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-20 18:40:08 +0530

    questions file

---
+++ loop_practice.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    // Q1. Write a 'while' to calculate the sum of 1 to 5
+
+    // Q2. Write a 'while' loop to count down from 5 to 1 and store in a vector named 'counter
+
+    // Q3. Write a 'do-while' loop to accept a number from user  until user says "no" keep accepting
+
+    // Q4. Write a 'do-while' loop that adds numbers from 1 to 4 and stores the answer in a vsriable named 'totalSum'
+
+    // Q5. Write a 'for' loop that multiplies each element of the array [2, 5, 8, 9] by 2 and stores the result in a new array named multipliedArray
+
+    // Q6. Write a 'for' loop that iterators of the following list [2,5,6,1,4,7] and stores each item into anoter list named 'newList'
+
+    // Q7. Write a 'for' loop that iterates over a vector [1,2,3,4,5,6,7,8,9] and stops the loop when 7 is found.
+
+    // Q8. Write a 'for' loop that iterates over a vector [1,2,3,4,5,6,7,8,9,10,11] and skips the loop when 3 is found.
+
+    // Q9. Write a 'for' loop that iterates over an array[2,5,6,7,8], skips 6 and multiplies rest by 2 and store in a new array named "doubleVale"
+
+    return 0;
+}
